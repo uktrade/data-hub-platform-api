efcf44b7edf972a6802c01e65323463172bd859a
Craig J. Bass <1889973+craigjbass@users.noreply.github.com>
2022-04-12 15:06:00 +0100

    Refactor: Rename to execute

--- test/test_contextual_actions.py
+++ test/test_contextual_actions.py
@@ -4,21 +4,21 @@ from platform_api.use_case_executor import UseCaseExecutor
 
 
 @pytest.fixture()
-def use_case_executor():
+def execute():
     executor = UseCaseExecutor()
     executor._redis.get_client().flushdb()
     yield executor
 
 
-def test_can_error_when_no_context_registered(use_case_executor):
-    response = use_case_executor('get_contextual_actions', 'dit:datahub:company')
+def test_can_error_when_no_context_registered(execute):
+    response = execute('get_contextual_actions', 'dit:datahub:company')
     assert not response['success']
     assert ['NO_ACTION_CONTEXT_DEFINED'] == response['errors']
 
 
-def test_can_get_no_contextual_actions(use_case_executor):
-    use_case_executor('register_action_context', 'dit:datahub:company')
-    response = use_case_executor('get_contextual_actions', 'dit:datahub:company')
+def test_can_get_no_contextual_actions(execute):
+    execute('register_action_context', 'dit:datahub:company')
+    response = execute('get_contextual_actions', 'dit:datahub:company')
     contextual_action = response['actions']
     assert response['success']
     assert [] == contextual_action
