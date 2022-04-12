ff6d2f8be8b478737fb042806d4c4cdba0982a16
Craig J. Bass <1889973+craigjbass@users.noreply.github.com>
2022-04-12 14:54:32 +0100

    Refactor: Extract a use case executor.

--- test/test_contextual_actions.py
+++ test/test_contextual_actions.py
@@ -5,29 +5,33 @@ from platform_api.redis_connection import RedisConnection
 from platform_api.use_case.get_contextual_actions import GetContextualActions
 from platform_api.use_case.register_action_context import RegisterActionContext
 
+class UseCaseExecutor:
+    def __init__(self):
+        self._redis = RedisConnection()
+        action_context_gateway = RedisActionContext(self._redis.get_client())
+        self.use_cases = {
+            'get_contextual_actions': GetContextualActions(action_context_gateway),
+            'register_action_context': RegisterActionContext(action_context_gateway)
+        }
+
+    def __call__(self, use_case, *args, **kwargs):
+        return self.use_cases[use_case](*args, **kwargs)
+
 
 @pytest.fixture(autouse=True)
 def before_each():
-    RedisConnection().get_client().flushdb()
+    UseCaseExecutor()._redis.get_client().flushdb()
 
 
 def test_can_error_when_no_context_registered():
-    action_context_gateway = RedisActionContext(RedisConnection().get_client())
-
-    get_contextual_actions = GetContextualActions(action_context_gateway)
-    response = get_contextual_actions('dit:datahub:company')
+    response = UseCaseExecutor()('get_contextual_actions', 'dit:datahub:company')
     assert not response['success']
     assert ['NO_ACTION_CONTEXT_DEFINED'] == response['errors']
 
 
 def test_can_get_no_contextual_actions():
-    action_context_gateway = RedisActionContext(RedisConnection().get_client())
-
-    register_action_context = RegisterActionContext(action_context_gateway)
-    register_action_context('dit:datahub:company')
-
-    get_contextual_actions = GetContextualActions(action_context_gateway)
-    response = get_contextual_actions('dit:datahub:company')
+    UseCaseExecutor()('register_action_context', 'dit:datahub:company')
+    response = UseCaseExecutor()('get_contextual_actions', 'dit:datahub:company')
     contextual_action = response['actions']
     assert response['success']
     assert [] == contextual_action
