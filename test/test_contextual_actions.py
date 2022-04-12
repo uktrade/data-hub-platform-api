caca8a00ab3c09dfa1c35ff2fd0bab3932cefb41
Craig J. Bass <1889973+craigjbass@users.noreply.github.com>
2022-04-12 14:55:25 +0100

    Refactor: Extract use case executor into file.

--- test/test_contextual_actions.py
+++ test/test_contextual_actions.py
@@ -1,21 +1,6 @@
 import pytest
 
-from platform_api.redis_action_context import RedisActionContext
-from platform_api.redis_connection import RedisConnection
-from platform_api.use_case.get_contextual_actions import GetContextualActions
-from platform_api.use_case.register_action_context import RegisterActionContext
-
-class UseCaseExecutor:
-    def __init__(self):
-        self._redis = RedisConnection()
-        action_context_gateway = RedisActionContext(self._redis.get_client())
-        self.use_cases = {
-            'get_contextual_actions': GetContextualActions(action_context_gateway),
-            'register_action_context': RegisterActionContext(action_context_gateway)
-        }
-
-    def __call__(self, use_case, *args, **kwargs):
-        return self.use_cases[use_case](*args, **kwargs)
+from platform_api.use_case_executor import UseCaseExecutor
 
 
 @pytest.fixture(autouse=True)
