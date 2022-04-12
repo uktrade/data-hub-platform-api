126331eeea8af6922827f8bf7f946815c1cdf99e
Craig J. Bass <1889973+craigjbass@users.noreply.github.com>
2022-04-12 15:04:39 +0100

    Refactor: Extract to fixture.

--- test/test_contextual_actions.py
+++ test/test_contextual_actions.py
@@ -3,20 +3,22 @@ import pytest
 from platform_api.use_case_executor import UseCaseExecutor
 
 
-@pytest.fixture(autouse=True)
-def before_each():
-    UseCaseExecutor()._redis.get_client().flushdb()
+@pytest.fixture()
+def use_case_executor():
+    executor = UseCaseExecutor()
+    executor._redis.get_client().flushdb()
+    yield executor
 
 
-def test_can_error_when_no_context_registered():
-    response = UseCaseExecutor()('get_contextual_actions', 'dit:datahub:company')
+def test_can_error_when_no_context_registered(use_case_executor):
+    response = use_case_executor('get_contextual_actions', 'dit:datahub:company')
     assert not response['success']
     assert ['NO_ACTION_CONTEXT_DEFINED'] == response['errors']
 
 
-def test_can_get_no_contextual_actions():
-    UseCaseExecutor()('register_action_context', 'dit:datahub:company')
-    response = UseCaseExecutor()('get_contextual_actions', 'dit:datahub:company')
+def test_can_get_no_contextual_actions(use_case_executor):
+    use_case_executor('register_action_context', 'dit:datahub:company')
+    response = use_case_executor('get_contextual_actions', 'dit:datahub:company')
     contextual_action = response['actions']
     assert response['success']
     assert [] == contextual_action
