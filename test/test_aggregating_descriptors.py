3d80bda97e4cb87cd8ca750377c48b3440aecc93
Craig J. Bass <1889973+craigjbass@users.noreply.github.com>
2022-04-19 14:41:29 +0100

    Refactor: Extract beginning of simulator class.

--- test/test_aggregating_descriptors.py
+++ test/test_aggregating_descriptors.py
@@ -4,6 +4,34 @@ from httpretty import httpretty, HTTPretty
 from platform_api.use_case_executor import UseCaseExecutor
 
 
+class ServiceSimulator:
+    def __init__(self, ping_mesh):
+        self.ping_mesh = ping_mesh
+        self._endpoint = None
+        self._id = None
+
+    def simulate(self, id_, endpoint):
+        self._endpoint = endpoint
+        self._id = id_
+        return self
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *_):
+        self._service = None
+        self._id = None
+        pass
+
+    def register(self, response):
+        self.ping_mesh(id_=self._id, href=self._endpoint)
+        httpretty.register_uri(
+            HTTPretty.GET,
+            self._endpoint,
+            body=response
+        )
+
+
 @pytest.fixture()
 def execute():
     execute = UseCaseExecutor()
@@ -12,9 +40,9 @@ def execute():
 
 
 @pytest.fixture()
-def service_simulator():
+def simulate(execute):
     httpretty.enable(allow_net_connect=False, verbose=True)
-    yield None
+    yield ServiceSimulator(lambda **kwargs: execute('ping_mesh', **kwargs)).simulate
     httpretty.disable()
 
 
@@ -29,46 +57,39 @@ def test_can_get_no_tagged_descriptors(execute):
     }
 
 
-def test_can_get_one_tagged_descriptor(execute, service_simulator):
-    execute('ping_mesh', id_='my-service', href='https://my-service.local/mesh')
-
-    my_service_response = {
-        'hypermedia': {
-            '_links': {
-                'dit:my-service:ReferForHelp': {
-                    'href': 'https://my-service.local/refer/~{companyId}',
-                    'method': 'get'
-                }
-            }
-        },
-        'semantics': {
-            "alps": {
-                "version": "1.0",
-                "id": "dit:my-service",
-                "descriptor": [
-                    {
-                        "name": "dit:my-service:ReferForHelp",
-                        "type": "safe",
-                        "doc": "Refer for Help",
-                        "tag": "dit:datahub:company",
-                        "descriptor": [
-                            {
-                                "id": "companyId",
-                                "name": "dit:datahub:company:id",
-                                "type": "semantic",
-                            }
-                        ]
+def test_can_get_one_tagged_descriptor(execute, simulate):
+    with simulate('my-service', 'https://my-service.local/mesh') as service:
+        service.register({
+            'hypermedia': {
+                '_links': {
+                    'dit:my-service:ReferForHelp': {
+                        'href': 'https://my-service.local/refer/~{companyId}',
+                        'method': 'get'
                     }
-                ]
-            }
-        },
-    }
-
-    httpretty.register_uri(
-        HTTPretty.GET,
-        'https://my-service.local/mesh',
-        body=my_service_response
-    )
+                }
+            },
+            'semantics': {
+                "alps": {
+                    "version": "1.0",
+                    "id": "dit:my-service",
+                    "descriptor": [
+                        {
+                            "name": "dit:my-service:ReferForHelp",
+                            "type": "safe",
+                            "doc": "Refer for Help",
+                            "tag": "dit:datahub:company",
+                            "descriptor": [
+                                {
+                                    "id": "companyId",
+                                    "name": "dit:datahub:company:id",
+                                    "type": "semantic",
+                                }
+                            ]
+                        }
+                    ]
+                }
+            },
+        })
 
     response = execute('get_tagged_descriptors', 'dit:datahub:company')
     assert response['hypermedia']['_links']['dit:my-service:ReferForHelp'] == {
