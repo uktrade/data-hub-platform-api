4412c0c0cfb7510353ef3433f09c8d9dca55f3f6
Craig J. Bass <1889973+craigjbass@users.noreply.github.com>
2022-04-21 14:14:37 +0100

    Refactor: Add get all method.

--- platform_api/http_node_profile.py
+++ platform_api/http_node_profile.py
@@ -5,5 +5,11 @@ class HttpNodeProfile:
     def __init__(self, mesh_nodes_gateway):
         self.mesh_nodes_gateway = mesh_nodes_gateway
 
+    def get_all(self):
+        profiles = []
+        for node in self.mesh_nodes_gateway.get_all():
+            profiles.append(self.to_profile(node))
+        return profiles
+
     def to_profile(self, node):
         return requests.get(node.href).json()
