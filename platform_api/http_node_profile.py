416fd67bc8b1a05ede047b7c0463e02be03e95bd
Craig J. Bass <1889973+craigjbass@users.noreply.github.com>
2022-04-21 14:07:26 +0100

    Refactor: Add dependency on mesh nodes gateway

--- platform_api/http_node_profile.py
+++ platform_api/http_node_profile.py
@@ -2,5 +2,8 @@ import requests
 
 
 class HttpNodeProfile:
+    def __init__(self, mesh_nodes_gateway):
+        self.mesh_nodes_gateway = mesh_nodes_gateway
+
     def to_profile(self, node):
         return requests.get(node.href).json()
